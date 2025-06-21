b5b4b5a1f510b24aeb60116c7f8b27abd18e449d
Chanaka-Prasanna <chanakapinfo@gmail.com>
2025-06-21 13:59:33 +0530

    creted docker files and tested locally, add healthcheck

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI(title="E commerce  Backend")
 @app.get('/')
 async def read_root():
     return {"message":"Hello, World from E commerce - Backend"}
+
+@app.get("/healthz")
+async def healthz():
+    return {"status": "ok"}
